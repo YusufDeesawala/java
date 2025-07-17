077e4c2648d5b87099f2cebdbdc7bf02aa8340e0
YusufDeesawala <yusufdeesawala72@gmail.com>
2025-07-17 14:52:12 +0530

    First program with vars.

---
+++ 17-7-25/main.java
@@ -0,0 +1,17 @@
+public class main{
+    public static void main(String[] args){
+        // This is my first java prog
+        /* This is a multi line comment */
+        System.out.println("I like java");
+        System.out.println("Its easy");
+        int age = 20;
+        System.out.println("I am " + age + " years old.");
+        double price = 12.76;
+        double temp  = 35.5;
+        System.out.println("The price of the product is " + price);
+        System.out.println("The temperature outside is " + temp);
+    }
+}
+
+// primitive = int,double,char,boolean
+// refernce = string, array, object
